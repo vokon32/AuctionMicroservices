7d1176acf47472629baf5dde4f4522d2593b26ad
Evgeniy <jenyleshenkoo@gmail.com>
2023-10-11 14:02:48 +0300

    addedNextjsReadingOperaions

---
+++ frontend/web-app/types/index.ts
@@ -0,0 +1,27 @@
+import { type } from "os"
+
+export type PagedResult<T> ={
+    results: T[]
+    pageCount: number
+    totalCount: number
+}
+
+export type Auction =  {
+    reservePrice: number
+    seller: string
+    winner?: string
+    soldAmount: number
+    currentHighBid: number
+    createdAt: string
+    updateAt: string
+    auctionEnd: string
+    status: string
+    make: string
+    model: string
+    year: number
+    color: string
+    mileage: number
+    imageUrl: string
+    id: string
+  }
+  
