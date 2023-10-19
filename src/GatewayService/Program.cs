2c96b5f38a96efe0e2fa440d0fc5b3165b7950e3
Evgeniy <jenyleshenkoo@gmail.com>
2023-10-19 14:31:49 +0300

    addedNotificationService

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,18 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
